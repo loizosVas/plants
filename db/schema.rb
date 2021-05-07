82799fed0642fd93adb1b3e5ac0b5dafcd4b6818
Loizos Vasileiou <loizos.vas@hotmail.com>
2021-05-07 23:29:52 +0300

    Set new column isAdmin to table users to determine if user is admin (default is set to false)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_171541) do
+ActiveRecord::Schema.define(version: 2021_05_07_201506) do
 
   create_table "plants", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2021_05_07_171541) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "isAdmin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
